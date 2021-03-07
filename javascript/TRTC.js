542004c443b2cbb4cbe86221d3a4a3b592303255
LAPTOP-P980Q3B7\User <ii60425332ii20@gmail.com>
2021-03-07 20:52:27 +0800

    no message

---
+++ javascript/TRTC.js
@@ -0,0 +1,24 @@
+let device = {
+  panel_borad: [
+    { name: "盤名", place: "位置", upstream: "上游" },
+    { name: "LP1", place: "aaa", upstream: "PA1" },
+    { name: "LP2", place: "bbb", upstream: "PA2" },
+    { name: "ERP4", place: "ccc", upstream: "PB3" },
+  ],
+  smoker: [
+    { place: "位置", loop: "迴路編號" },
+    { place: "3F", loop: "L1-111" },
+    { place: "2F", loop: "L1-222" },
+    { place: "4F", loop: "L2-14" },
+  ],
+};
+
+let app = new Vue({
+  el: "#app",
+  data: {
+    input: "",
+    device_sort: "全部",
+    device: device,
+    time: new Date().toLocaleString(),
+  },
+});

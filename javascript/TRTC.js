557f2babda5bad85763110172f1006c3de940860
LAPTOP-P980Q3B7\User <ii60425332ii20@gmail.com>
2021-03-10 20:12:16 +0800

    no message

--- javascript/TRTC.js
+++ javascript/TRTC.js
@@ -14,9 +14,73 @@ let device = {
 };
 
 let Circular_line = {
+  sss: "./images/smoker/1F-1.png",
   station: [
     {
-      Y7: [{ station_name: "十四張", device: [{ water: "aaa", fire: "sss" }] }],
+      Y13: [
+        {
+          station_name: "中原",
+          device: [
+            {
+              device_water: "水電設備",
+              device_fire: "消防設備",
+              water: [
+                {
+                  panel_borad: [
+                    {
+                      name: "盤名",
+                      place: "位置",
+                      place_image: "",
+                      upstream: "上游",
+                    },
+                    {
+                      name: "LP1",
+                      place: "aaa",
+                      place_image: "",
+                      upstream: "PA1",
+                    },
+                    {
+                      name: "LP2",
+                      place: "bbb",
+                      place_image: "",
+                      upstream: "PA2",
+                    },
+                    {
+                      name: "ERP4",
+                      place: "ccc",
+                      place_image: "",
+                      upstream: "PB3",
+                    },
+                  ],
+                  panel_borad_map: [
+                    {
+                      floor1: "",
+                      floor2: "",
+                      floor3: "",
+                      floor4: "",
+                      floor5: "",
+                      B1: "",
+                    },
+                  ],
+                  pump: "",
+                  ground: "",
+                },
+              ],
+              fire: [
+                {
+                  smoker: [
+                    { place: "位置", loop: "迴路編號" },
+                    { place: "3F", loop: "L1-111" },
+                    { place: "2F", loop: "L1-222" },
+                    { place: "4F", loop: "L2-14" },
+                  ],
+                  water_system: [{}],
+                },
+              ],
+            },
+          ],
+        },
+      ],
     },
   ],
 };
@@ -25,7 +89,8 @@ let app = new Vue({
   data: {
     input: "",
     device_sort: "全部",
-    device: device,
+    device: Circular_line,
+
     time: new Date().toLocaleString(),
   },
 });
